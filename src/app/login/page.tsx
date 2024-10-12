82610d67de4ed1459ddfa37202527fecefcf38c1
BilalSohailModi <bilalsohailmodi1@gmail.com>
2024-10-12 20:57:22 +0500

    feat: add login and signup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "../styles/SignIn.module.css";
+import { login, me } from "../services/authService";
+
+import { useRouter } from "next/navigation";
+import { useAtom } from "jotai";
+import { userState } from "../jotai/user.jotai";
+import Link from "next/link";
+
+const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const [, setUser] = useAtom(userState);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await me();
+        setUser(response);
+        router.push("/movies");
+      } catch (error) {}
+    };
+
+    checkAuth();
+  }, [router]);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Handle form submission logic (e.g., API call)
+    console.log({ email, password });
+    try {
+      const user = await login({ email, password });
+
+      console.log("Login successful", user);
+
+      router.push("/movies");
+      // Redirect user or save the auth token as needed
+    } catch (error: any) {
+      console.error(error?.message);
+      // Handle error (show error message)
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>Sign in</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className={styles.inputGroup}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className={styles.rememberMe}>
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+          </div>
+
+          <button className={styles.loginButton} type="submit">
+            Login
+          </button>
+        </form>
+        <p>
+          Don't have an account?
+          <Link href="/signUp" style={{ marginTop: "10px" }}>
+            Create Account
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/signUp/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+// src/pages/signup.tsx
+import React, { useEffect, useState } from "react";
+import styles from "../styles/SignUp.module.css"; // Importing CSS module
+import { me, signup } from "../services/authService";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const SignUp: React.FC = () => {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const router = useRouter();
+  const [error, setError] = useState<string>("");
+  const [showError, setShowError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await me();
+        router.push("/movies");
+      } catch (error) {}
+    };
+
+    checkAuth();
+  }, [router]);
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Handle sign-up logic here
+    try {
+      if (password !== confirmPassword) {
+        alert("passwords not matched");
+        throw { message: "passwords not matched" };
+      }
+      const user = await signup({
+        email,
+        password,
+        firstName,
+        lastName,
+      });
+      console.log("SignUp successful", user);
+      router.push("/movies");
+    } catch (error: any) {
+      console.error(error?.message);
+      setError(error?.message || "something went wrong");
+      setShowError(true);
+
+      // Handle error (show error message)
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Sign Up</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="First Name"
+          className={styles.input}
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className={styles.input}
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className={styles.input}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className={styles.input}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className={styles.input}
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        {showError ? <p className={styles.error}>{error}</p> : <></>}
+        <button type="submit" className={styles.button}>
+          Sign Up
+        </button>
+      </form>
+
+      <p>
+        Already have an account?
+        <Link href="/login">Login</Link>
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
